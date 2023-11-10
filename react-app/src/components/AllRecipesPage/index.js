449134bcc02011c71a52a8ed2a5b3a6686795f8d
Jeff <jeffyoon48@gmail.com>
2023-11-09 21:10:19 -0800

    started on all recipes page; switching task to redux store

---
+++ react-app/src/components/AllRecipesPage/index.js
@@ -0,0 +1,19 @@
+// import React, { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+
+// function AllRecipesPage() {
+//     const dispatch = useDispatch()
+
+//     useEffect(() => {
+
+//     })
+
+//     return (
+//         <div id="all-recipes-page">
+//             <h1>Explore all recipes</h1>
+//             <div className="recipe-wrapper">
+
+//             </div>
+//         </div>
+//     )
+// }
