3b26c7290669278b1c442588c5dc925fab22e13e
Jeff <jeffyoon48@gmail.com>
2023-12-04 16:42:46 -0800

    model edit

--- app/models/recipe_instruction.py
+++ app/models/recipe_instruction.py
@@ -5,14 +5,19 @@ class RecipeInstruction(db.Model):
   __tablename__ = 'recipe_instructions'
 
   if environment == "production":
-    __table_args__ = {'schema': SCHEMA}
+    __table_args__ = (
+      {'schema': SCHEMA},
+      UniqueConstraint('recipe_id', 'instruction_num', name='uq_recipe_instructions'),
+    )
+  else:
+    __table_args__ = (
+      UniqueConstraint('recipe_id', 'instruction_num', name='uq_recipe_instructions'),
+    )
 
   id = db.Column(db.Integer, primary_key=True)
   recipe_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("recipes.id")), nullable=False)
   desc = db.Column(db.String, nullable=False)
   instruction_num = db.Column(db.Integer, nullable=False)
-
-  __table_args__ = (UniqueConstraint('recipe_id', 'instruction_num', name='uq_recipe_instructions'),)
 
   recipe = db.relationship('Recipe', back_populates='recipe_instructions')
 
